fac444c9bd44720cceb5f7d4b5ee7025b3914df3
Nicolas Cretté <nico.crette@gmail.com>
2016-11-21 14:38:19 +0100

    Update and rename menu.html to menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<section class="page-header">
+     <h1 class="project-name">Projet PJS3</h1>
+     <h2 class="project-tagline"></h2>
+     <a href="https://github.com/Blackkey5/pjs3_project" class="btn">Accueil</a>
+     <a href="https://github.com/Blackkey5/pjs3_project/" class="btn">Réservation</a>
+     <a href="https://github.com/Blackkey5/pjs3_project/" class="btn">Entreprise</a>
+     <a href="https://github.com/Blackkey5/pjs3_project/" class="btn">Aventure</a>
+</section>
